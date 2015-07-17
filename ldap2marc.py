8b736512b508dcad1803f4b911f1a95849fe59e9
Jochen Klein <klein.jochen@gmail.com>
2015-07-17 15:35:00 +0200

    json: LDAP record to json-formatted file export
    * Adds terminal parameter `--exportjson` to export and save LDAP
      records to a json-formatted file using Python `json` module.
    
    * Renames `--writexml` to `--exportxml` to have the same name
      convention.

--- ldap2marc.py
+++ ldap2marc.py
@@ -2,6 +2,8 @@ import datetime
 from optparse import OptionParser
 import ldap_cern
 from mapper import Mapper
+import json
+
 
 parser = OptionParser()
 parser.add_option(
@@ -20,11 +22,18 @@ parser.add_option(
     help="record element size for each XML file [default: %default]")
 parser.add_option(
     "-x",
-    "--writexml",
-    dest="writexml",
+    "--exportxml",
+    dest="exportxml",
     action="store_true",
     default=False,
-    help="write to XML file(s): [default: %default]")
+    help="write to XML file(s) [default: %default]")
+parser.add_option(
+    "-j",
+    "--exportjson",
+    dest="exportjson",
+    action="store_true",
+    default=False,
+    help="dump Python dictionary with records to file [default: %default]")
 (options, args) = parser.parse_args()
 
 t0 = datetime.datetime.now()
@@ -34,7 +43,11 @@ t_final = t1 - t0
 print "Entries found: {0}, Time [seconds]: {1}.{2}".format(
     len(all_results), t_final.seconds, t_final.microseconds/1000)
 
-if options.writexml:
+if options.exportxml:
     mapper = Mapper(options.recordsize)  # mapper with given record size
     mapper.map_ldap_records(all_results)
     mapper.write_marcxml()
+
+if options.exportjson:
+    with open("save.json", "w") as f:
+        json.dump([x for _, x in all_results], f)

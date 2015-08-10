83008990eea12668ce162cf6f7af1c8fc5768386
Jochen Klein <klein.jochen@gmail.com>
2015-08-10 11:18:36 +0200

    mapper: global improvement
    * Fixes issue, if a LDAP record doesn't contain the requested
      attribute.
    
    * Removes unused parameter in function `write_marcxml`.
    
    * Improves function consistency.
    
    * Improves docstring and Python styling.

--- mapper.py
+++ mapper.py
@@ -5,12 +5,13 @@ import os
 
 class Mapper:
     """Map LDAP records to MARC 21 authority records in XML sctructure.
-    MARC 21 authority reference: http://www.loc.gov/marc/authority/"""
+    MARC 21 authority reference: http://www.loc.gov/marc/authority/
+    """
     def __init__(self, record_size=500):
         """Initialize the mapper properties."""
-        self.roots = []  # contains all root elements
-        self.record_size = record_size  # defines amount of record elements in one root
-        self.mapper_dict = {  # mapping definition LDAP attributes to MARC 21 authority
+        self.roots = []  # contain all root elements
+        self.record_size = record_size  # amount of record elements in root
+        self.mapper_dict = {  # mapping definition LDAP to MARC 21 authority
             "givenName": "1000_a",
             "sn": "1001_a",
             "displayName": "100__a",
@@ -19,12 +20,15 @@ class Mapper:
         }
 
     def _strip(self, obj):
-        """Strip the property string and return it. Using the default strip function."""
+        """Strip the property string and return it.
+        Using the default strip function.
+        """
         return str(obj).strip("['']")
 
     def _split_marc_id(self, marc_id):
-        """Seperate marc identifier which is defined in the mapper
-        dictionary. Return tag, ind1, ind2 and code."""
+        """Seperate MARC 21 identifier which is defined in the mapper_dict.
+        Return tag, ind1, ind2 and code.
+        """
         # MARC code is optional
         try:
             marc_code = marc_id[5]
@@ -33,64 +37,84 @@ class Mapper:
 
         return marc_id[:3], marc_id[3], marc_id[4], marc_code
 
-    def _add_root(self):
-        """Add the root element `collection` with the attribute `xmlns`.
-        Append it to the roots list."""
-        self.root = etree.Element(
+    def _create_root(self):
+        """Create root element 'collection' with the attribute 'xmlns' and
+        return it.
+        """
+        return etree.Element(
             "collection", {"xmlns": "http://www.loc.gov/MARC21/slim"})
-        self.roots.append(self.root)
 
-    def _add_record(self):
-        """Add sub element `record` to the root element."""
+    def _create_record(self):
+        """Create child element 'record', add it to the root element and
+        return it.
+        """
         return etree.SubElement(self.root, "record")
 
-    def _add_controlfield(self, attr_tag, text):
-        """Add sub element `controlfield` to the root element."""
-        etree.SubElement(self.root, "controlfield", {
-            "tag": attr_tag}).text = text
+    def _create_controlfield(self, attr_tag, inner_text):
+        """Create child element 'controlfield', add it to the root element and
+        return it.
+        """
+        controlfield = etree.SubElement(self.root, "controlfield", {
+            "tag": attr_tag})
+        controlfield.text = inner_text
+        return controlfield
 
-    def _add_datafield(
+    def _create_datafield(
       self, attr_tag, attr_ind1, attr_ind2, repeatable=False):
-        """Add datafield element as a sub element to the record element and
-        return it. In case a datafield with the same tag exists, the existing
-        one will be returned. Enable `repeatable` if you have other sources and
-        want to have repeatable datafield elements."""
+        """Create child element 'datafield', add it to the record element and
+        return it.
+
+        :param bool repeatable: Allows multiple datafields with same tags
+        """
         if attr_ind1 == "_":
             attr_ind1 = " "
         if attr_ind2 == "_":
             attr_ind2 = " "
 
         find = self.record.xpath(
-            "datafield[@tag=%s and @ind1='%s' and @ind2='%s']" % (attr_tag, attr_ind1, attr_ind2))
+            "datafield[@tag=%s and @ind1='%s' and @ind2='%s']"
+            % (attr_tag, attr_ind1, attr_ind2))
         if not find or repeatable:
             return etree.SubElement(self.record, "datafield", OrderedDict({
                 "tag": attr_tag, "ind1": attr_ind1, "ind2": attr_ind2}))
         return find[0]
 
-    def _add_subfield(self, elem_datafield, attr_code, text):
-        """Add child element 'subfield' to the datafield with attributes and
-        inner text."""
-        etree.SubElement(elem_datafield, "subfield", {
-            "code": attr_code}).text = text
+    def _create_subfield(self, elem_datafield, attr_code, inner_text):
+        """Create child element 'subfield' of elem_datafield including
+        attr_code and inner_text, and return it.
+        """
+        subfield = etree.SubElement(
+            elem_datafield, "subfield", {"code": attr_code})
+        subfield.text = inner_text
+        return subfield
 
     def map_ldap_records(self, ldap_records):
-        """Map LDAP records to Marc 21 XML sctructure (XML tree) and return root node.
-        Example LDAP record: ('CN=joklein,OU=Users,OU=Organic Units,DC=cern,DC=ch',
-            {'mail': ['j.klein@cern.ch'], 'givenName': ['Jochen'], 'displayName': ['Jochen Klein'], 'sn': ['Klein']})"""
-        self._add_root()
+        """Map LDAP records to MARC 21 authority records (XML).
+        Return the list of root elements.
+        """
+        self.root = self._create_root()
+        self.roots.append(self.root)
         record_size_counter = 0
 
         for record in ldap_records:
             if record_size_counter == self.record_size:
-                self._add_root()
+                self.root = self._create_root()
+                self.roots.append(self.root)
                 record_size_counter = 0  # reset counter
             record_size_counter += 1
-            self.record = self._add_record()
+            self.record = self._create_record()
             for attr_key in self.mapper_dict.keys():
                 marc_id = self.mapper_dict[attr_key]
-                marc_tag, marc_ind1, marc_ind2, marc_code = self._split_marc_id(marc_id)
-                inner_text = self._strip(record[1][attr_key])
-                elem_datafield = self._add_datafield(
+                marc_tag, marc_ind1, marc_ind2, marc_code = \
+                    self._split_marc_id(marc_id)
+
+                # in case `attr_key` doesn't exist in the LDAP record
+                try:
+                    inner_text = self._strip(record[attr_key])
+                except:
+                    pass
+
+                elem_datafield = self._create_datafield(
                     marc_tag, marc_ind1, marc_ind2)
 
                 # add prefixes for specific codes
@@ -98,14 +122,15 @@ class Mapper:
                     inner_text = "AUTHOR|(SzGeCERN)%s" % inner_text
 
                 if marc_code:
-                    self._add_subfield(elem_datafield, marc_code, inner_text)
-        return self.root
+                    self._create_subfield(
+                        elem_datafield, marc_code, inner_text)
+        return self.roots
 
-    def write_marcxml(self, root_node=None, file="marc_output.xml"):
+    def write_marcxml(self, file="marc_output.xml"):
         """Write the XML tree to file(s)."""
-        if not root_node:
-            root_node = self.root
         filename, file_extension = os.path.splitext(file)
+
+        # multiple file output
         for i, root in enumerate(self.roots):
             with open("%s_%03d.xml" % (filename, i), "w") as f:
                 f.write(etree.tostring(root, pretty_print=True))

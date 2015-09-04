e66ce174d31654bfcc5820324e24a3b5f11aedfc
Jochen Klein <klein.jochen@gmail.com>
2015-09-04 11:47:12 +0200

    ldap2marc: argparse
    * Replaces optparse with argparse (Python version 2.7+).

--- ldap2marc.py
+++ ldap2marc.py
@@ -1,71 +1,71 @@
-from optparse import OptionParser
+import argparse
 from os.path import isfile
 import json
 import ldap_cern
 from mapper import Mapper
 import utils
 
-parser = OptionParser()
-parser.add_option(
+parser = argparse.ArgumentParser()
+parser.add_argument(
     "-p",
     "--pagesize",
     dest="pagesize",
-    type="int",
+    type=int,
     default=250,
     help="limit LDAP records for each search request to avoid exceeding "
-         "sizelimit [default: %default]")
-parser.add_option(
+         "sizelimit [default: %(default)d]")
+parser.add_argument(
     "-r",
     "--recordsize",
     dest="recordsize",
-    type="int",
+    type=int,
     default=500,
     help="limit amount of record elements for each XML file and has to be "
-         "combined with `-x FILE` [default: %default]")
-parser.add_option(
+         "combined with `-x FILE` [default: %(default)d]")
+parser.add_argument(
     "-x",
     "--exportxml",
     dest="exportxml",
-    type="string",
+    type=str,
     metavar="FILE",
     help="write to XML FILE(s) based on `recordsize`")
-parser.add_option(
+parser.add_argument(
     "-j",
     "--exportjson",
     dest="exportjson",
-    type="string",
+    type=str,
     metavar="FILE",
     help="dump Python dictionary with records to a json-formatted FILE")
-parser.add_option(
+parser.add_argument(
     "-u",
     "--update",
     dest="update",
-    type="string",
+    type=str,
     metavar="FILE",
     help="update stored json-formatted records with current LDAP records "
          "to FILE")
-(options, args) = parser.parse_args()
+args = parser.parse_args()
 
-all_results = ldap_cern.paged_search(options.pagesize)
+all_results = ldap_cern.paged_search(args.pagesize)
 print "Records found: {0}.".format(len(all_results))
 
-if options.exportxml:
+if args.exportxml:
     mapper = Mapper()
     mapper.map_ldap_records(all_results)
-    mapper.write_marcxml(options.recordsize, options.exportxml)
+    mapper.write_marcxml(args.recordsize, args.exportxml)
 
-if options.exportjson:
-    utils.export_json(all_results, options.exportjson)
+if args.exportjson:
+    utils.export_json(all_results, args.exportjson)
 
-if options.update:
-    if isfile(options.update):
-        with open(options.update) as f:
+if args.update:
+    if isfile(args.update):
+        with open(args.update) as f:
             stored = json.load(f)
 
         records_diff = utils.diff_records(all_results, stored)
         if len(records_diff):
             # Update stored json-formatted records
-            utils.export_json(all_results, options.update)
+            utils.export_json(all_results, args.update)
 
             # Map updated LDAP records
             mapper = Mapper()
@@ -76,4 +76,4 @@ if options.update:
         else:
             print "No changes found."
     else:
-        print "updating failed. file '{0}' not found".format(options.update)
+        print "updating failed. file '{0}' not found".format(args.update)
